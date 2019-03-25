57c28c6e18139ed8b2c84d924fedec75ea8d8c3e
araws <45522139+araws@users.noreply.github.com>
2019-03-25 21:48:07 +0100

    Add files via upload
    Menu of difficulties

---
+++ Menu.java
@@ -0,0 +1,45 @@
+package projects.monsterGame;
+
+class Menu {
+
+    public enum GameDifficulty {
+        EASY (25, 15, 10, 4, 3),
+        MEDIUM (35, 18, 20, 3, 2),
+        HARD (50, 23, 30, 2, 1);
+
+        private  int boardWidth;
+        private  int boardHeight;
+        private  int numberOfMonster;
+        private  int numberOfPlayerLives;
+        private  int numberOfJumps;
+
+
+        GameDifficulty(int boardWidth, int boardHeight, int numberOfMonster, int numberOfPlayerLives, int numberOfJumps) {
+            this.boardWidth = boardWidth;
+            this.boardHeight = boardHeight;
+            this.numberOfMonster = numberOfMonster;
+            this.numberOfPlayerLives = numberOfPlayerLives;
+            this.numberOfJumps = numberOfJumps;
+        }
+
+        public int getBoardWidth() {
+            return boardWidth;
+        }
+
+        public int getBoardHeight() {
+            return boardHeight;
+        }
+
+        public int getNumberOfMonster() {
+            return numberOfMonster;
+        }
+
+        public int getNumberOfPlayerLives() {
+            return numberOfPlayerLives;
+        }
+
+        public int getNumberOfJumps() {
+            return numberOfJumps;
+        }
+    }
+}
