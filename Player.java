9e3b357b87fcd009e00f8e19e8114234219e38c4
araws <rawski.andrzej@gmail.com>
2019-06-06 00:08:59 +0200

    small refactor

--- Player.java
+++ Player.java
@@ -2,10 +2,6 @@ package monsterGame;
 
 public class Player extends Character {
 
-    Player() {
-
-    }
-
     Player(int liveQuantity, int positionX, int positionY) {
         super( liveQuantity, positionX, positionY );
     }
